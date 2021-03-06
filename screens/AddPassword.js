8d831733b9e4a40e70884ed6542fe6bb10b90d8c
Sravya2007 <sravyamarepally@outlook.com>
2021-03-06 11:17:34 +0530

    Project 93

--- screens/AddPassword.js
+++ screens/AddPassword.js
@@ -1,8 +1,90 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, Modal, ScrollView, TextInput, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
-import { Header, Icon } from 'react-native-elements';
+import { Header, Icon, Avatar, Input } from 'react-native-elements';
+import firebase from 'firebase';
+import db from '../config';
+import * as SecureStore from 'expo-secure-store';
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default class AddPassword extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userEmail: firebase.auth().currentUser.email,
+            accountName: '',
+            password: '',
+            image: '#'
+        }
+    }
+
+    savePassword = async(accountName, password, image) =>{
+        if(this.state.accountName && this.state.password) {
+            db.collection("user_accounts").add({
+                email: this.state.userEmail,
+                accountName: accountName,
+                image: image
+            })
+
+            await SecureStore.setItemAsync(accountName, password)
+
+            this.setState({
+                accountName: '',
+                password: '',
+                image: '#'
+            })
+            
+            Alert.alert('Your password was saved successfully!')
+        } else {
+            Alert.alert('Please fill all the fields.')
+        }
+    }
+
+    selectPicture = async() =>{
+        const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1
+        })
+    
+        if (!cancelled) {
+            this.uploadImage(uri, this.state.userEmail);
+        }
+    }
+
+    uploadImage = async(uri, imageName) =>{
+        var response = await fetch(uri)
+        var blob = await response.blob()
+        var ref = firebase
+        .storage()
+        .ref()
+        .child("account_pictures/" + imageName)
+
+        return ref.put(blob).then((response) => {
+            this.fetchImage(imageName)
+        })
+    }
+
+    fetchImage = (imageName) =>{
+        var storageRef = firebase
+        .storage()
+        .ref()
+        .child("account_pictures/" + imageName)
+        storageRef
+        .getDownloadURL()
+        .then((url) => {
+            this.setState({
+                image: url
+            })
+        })
+        .catch((error) => {
+            this.setState({
+                image: "#"
+            })
+        })
+    }
+
     render() {
         return(
             <View style = {styles.container}>
@@ -17,7 +99,62 @@ export default class AddPassword extends React.Component {
                 }
                 centerComponent={{ text: 'Add Password', style: {color: '#ffffff', fontSize: 30, fontWeight: "bold"} }}
                 backgroundColor = "#1DD91A"/>
-                <Text style = {{alignSelf: 'center', fontSize: 20, color: '#fff', fontWeight: 'bold', marginTop: 300}}>Add Password</Text>
+
+                <Input
+                  containerStyle = {{marginTop: 15}}
+                  leftIcon = {{ type: 'feather', name: 'user', size: 30, color: '#D9361A' }}
+                  placeholder = 'Eg: Google Account'
+                  placeholderTextColor = '#841DFA'
+                  inputStyle = {{fontSize: 23, color: '#841DFA'}}
+                  inputContainerStyle = {{borderColor: '#1DD91A', borderBottomWidth: 1.5}}
+                  label = 'Account Name'
+                  labelStyle = {{fontSize: 20, color: '#D9361A'}}
+                  onChangeText = {(text)=>{
+                    this.setState({
+                      accountName: text
+                    })
+                  }}
+                  value = {this.state.accountName}
+                />
+
+                <Input
+                  leftIcon = {{ type: 'feather', name: 'lock', size: 30, color: '#D9361A' }}
+                  placeholder = "Enter your account's password"
+                  placeholderTextColor = '#841DFA'
+                  inputStyle = {{fontSize: 23, color: '#841DFA'}}
+                  inputContainerStyle = {{borderColor: '#1EABFC', borderBottomWidth: 1.5}}
+                  secureTextEntry = {true}
+                  label = 'Password'
+                  labelStyle = {{fontSize: 20, color: '#D9361A'}}
+                  onChangeText = {(text)=>{
+                    this.setState({
+                      password: text
+                    })
+                  }}
+                  value = {this.state.password}
+                />
+
+                <Avatar
+                rounded
+                source = {{
+                    uri: this.state.image
+                }}
+                size = {"xlarge"}
+                onPress = {() => this.selectPicture()}
+                containerStyle = {{alignSelf: 'center'}}
+                placeholderStyle = {{backgroundColor: '#D9361A'}}
+                icon = {{name: "user", type: "feather"}}
+                >
+                <Avatar.Accessory size = {50} style = {{backgroundColor: '#841DFA'}}/>
+                </Avatar>
+
+                <TouchableOpacity
+                style = {styles.save}
+                onPress = {() => {
+                    this.savePassword(this.state.accountName, this.state.password, this.state.image)
+                }}>
+                    <Text style = {styles.buttonText}>SAVE</Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -27,5 +164,20 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#F0C529',
         flex: 1
+    },
+    save: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 330,
+        height: 50,
+        backgroundColor: '#1DD91A',
+        marginTop: 30
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: 'bold',
+      alignSelf: 'center'
     }
 })
--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,111 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert, Modal, ScrollView, TextInput, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { StyleSheet, Text, View, Alert, Modal, ScrollView, TextInput, TouchableOpacity, Image, KeyboardAvoidingView, FlatList } from 'react-native';
+import { Icon, ListItem, Avatar } from 'react-native-elements';
 import MyHeader from '../components/MyHeader';
+import firebase from 'firebase';
+import db from '../config';
 
 export default class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userEmail: firebase.auth().currentUser.email,
+            allAccounts: []
+        }
+        this.accountRef = null;
+    }
+
+    getAllAccounts = () =>{
+        this.accountRef = db.collection("user_accounts")
+        .where("email", "==", this.state.userEmail)
+        .onSnapshot((snapshot) =>{
+            var allAccounts = snapshot.docs.map(document => document.data());
+            this.setState({
+                allAccounts: allAccounts
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getAllAccounts()
+    }
+
+    keyExtractor = (item, index) => index.toString()
+
+    renderItem = ( {item, i} ) =>{
+        return (
+            <ListItem key = {i} bottomDivider containerStyle = {{margin: 20, width: 165, height: 165, flexDirection: 'column'}}>
+                <Avatar
+                rounded
+                source = {{
+                    uri: item.image
+                }}
+                size = {70}
+                containerStyle = {{alignSelf: 'center'}}
+                placeholderStyle = {{backgroundColor: '#D9361A'}}
+                icon = {{name: "user", type: "feather"}}
+                />
+                <ListItem.Content>
+                    <ListItem.Title
+                    style = {{color: '#1EABFC', fontWeight: 'bold', fontSize: 20}} 
+                    onPress ={()=>{
+                        this.props.navigation.navigate("SeePassword")
+                    }}>{item.accountName}</ListItem.Title>
+                </ListItem.Content>
+                <TouchableOpacity
+                style = {styles.view}>
+                </TouchableOpacity>
+          </ListItem>
+        )
+    }
+
     render() {
         return(
             <View style = {styles.container}>
                 <MyHeader title = "Home" navigation = {this.props.navigation}/>
-                <Text style = {{alignSelf: 'center', fontSize: 20, color: '#fff', fontWeight: 'bold', marginTop: 300}}>Home</Text>
-                <TouchableOpacity
-                style = {styles.addButton}
-                onPress = {() =>{
-                    this.props.navigation.navigate('AddPassword')
-                }}>
-                    <Icon name = "plus" type = "feather" color = "#fff" size = {30}/>
-                </TouchableOpacity>
-                <View style = {styles.addView}>
-                    <Text style = {styles.addText}>Add Password</Text>
+                <View style = {styles.container}>
+                {
+                    this.state.allAccounts.length === 0
+                    ?(
+                        <View>
+                        <Image
+                        source = {require('../assets/NoPassword.png')}
+                        style = {styles.image}/>
+                        <Text style = {styles.noPwd}>You have no saved passwords</Text>
+                        <Text style = {styles.noPwd}>Add a password to save it</Text>
+                        <TouchableOpacity
+                        style = {styles.addButton}
+                        onPress = {() =>{
+                            this.props.navigation.navigate('AddPassword')
+                        }}>
+                            <Icon name = "plus" type = "feather" color = "#fff" size = {30}/>
+                        </TouchableOpacity>
+                        <View style = {styles.addView}>
+                            <Text style = {styles.addText}>Add Password</Text>
+                        </View>
+                        </View>
+                    )
+                    :(
+                        <View>
+                        <FlatList
+                            keyExtractor = {this.keyExtractor}
+                            data = {this.state.allAccounts}
+                            renderItem = {this.renderItem}
+                            numColumns = {2}
+                        />
+                        <TouchableOpacity
+                        style = {styles.addButton}
+                        onPress = {() =>{
+                            this.props.navigation.navigate('AddPassword')
+                        }}>
+                            <Icon name = "plus" type = "feather" color = "#fff" size = {30}/>
+                        </TouchableOpacity>
+                        <View style = {styles.addView}>
+                            <Text style = {styles.addText}>Add Password</Text>
+                        </View>
+                        </View>
+                    )
+                }
                 </View>
             </View>
         )
@@ -36,19 +124,32 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         justifyContent: 'center',
         alignSelf: 'flex-end',
-        marginTop: 350
+        position: 'absolute',
+        marginTop: 680
     },
     addView: {
         backgroundColor: '#fff',
         width: 110,
         height: 30,
         borderRadius: 5,
-        marginVertical: -40,
+        marginVertical: 690,
         justifyContent: 'center',
-        marginLeft: 240
+        marginLeft: 240,
+        position: 'absolute'
     },
     addText: {
         alignSelf: 'center',
         color: '#1EABFC'
+    },
+    image: {
+      width: 350,
+      height: 350,
+      alignSelf: 'center'
+    },
+    noPwd: {
+        fontSize: 22,
+        color: '#841DFA',
+        alignSelf: 'center',
+        marginTop: 20
     }
 })
